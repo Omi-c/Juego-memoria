4df3b1d8841e4b656b6980f1f254519cd5efd14f
Omi <omaris.ti@gmail.com>
2024-08-19 22:05:45 -0400

    Se añade el proyecto

---
+++ src/data.js
@@ -0,0 +1,51 @@
+//Imagenes de las cartas 
+export const imgs = [
+	{
+		id: 1,
+		img: '/img/abeja.png',
+		alt: 'Abeja',
+	},
+	{
+		id: 2,
+		img: '/img/ajolote.png',
+		alt: 'Ajolote',
+	},
+	{
+		id: 3,
+		img: '/img/tortuga.png',
+		alt: 'Tortuga',
+	},
+	{
+		id: 4,
+		img: '/img/cangrejo.png',
+		alt: 'Cangrejo',
+	},
+	{
+		id: 5,
+		img: '/img/gato.png',
+		alt: 'Gato',
+	},
+	{
+		id: 6,
+		img: '/img/pollo.png',
+		alt: 'Pollo',
+	},
+	{
+		id: 7,
+		img: '/img/pajaro.png',
+		alt: 'Pajaro',
+	},
+	{
+		id: 8,
+		img: '/img/rana.png',
+		alt: 'Rana',
+	},
+	{
+		id: 9,
+		img: '/img/mariposa.png',
+		alt: 'Mariposa',
+	},
+	{ 	id: 10, 
+		img: '/img/pinguino.png', 
+		alt: 'Pinguino' },
+];
